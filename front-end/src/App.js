ba2bba401be5a9309a97043f8b0f8316c908c5b0
Marcos Antonio <marcos-rj@outlook.com>
2021-02-03 19:11:33 -0300

    primeiros passos em react e node

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VLibras from './components/vlibras';
+
+function App() {
+  return (
+    <div>
+     <h1>Conatus!</h1>
+     <VLibras />
+    </div>
+  );
+}
+
+export default App;
