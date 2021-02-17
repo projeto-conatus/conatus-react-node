5791daa67c059139425e3a7b2c8200387eebff72
nathaliadv <nathaliadantasv@gmail.com>
2021-02-17 11:36:33 -0300

    alteraçoes no model colaborador - adicionando validacoes

--- back-end/src/models/Colaborador.js
+++ back-end/src/models/Colaborador.js
@@ -12,7 +12,7 @@ class Colaboradores {
 
     selectPorNome(req, res) {
         db.query(
-            `SELECT * FROM colaborador WHERE nome = ${this.nome}`,
+            `SELECT * FROM colaborador WHERE nome = '${this.nome}'`,
             (error, result) => {
                 error ? res.send(error) : res.json(result);
             }
@@ -21,7 +21,7 @@ class Colaboradores {
 
     selectPorID(req, res) {
         db.query(
-            `SELECT * FROM colaborador WHERE id = ${this.id}`,
+            `SELECT * FROM colaborador WHERE id = '${this.id}'`,
             (error, result) => {
                 error ? res.send(error) : res.json(result);
             }
@@ -29,12 +29,31 @@ class Colaboradores {
     }
 
     insertColaborador(req, res) {
-        db.query(
-            `INSERT INTO colaborador (nome, senha) VALUES ('${this.nome}', '${this.senha}')`,
-            (error, result) => {
-                error ? res.send(error) : res.json('Novo colaborador cadastrado com sucesso!');
-            }
-        );
+        const erros= [];
+
+        if(!this.nome || !this.senha) {
+            erros.push({ mensagem : "Por favor preencha todos os campos"})
+
+        }
+
+        if (this.nome.length < 6) {
+            erros.push({ mensagem: "Nome de usuário deve ter, pelo menos, 6 dígitos" })
+        }
+
+        if (this.senha.length <6) {
+            erros.push({ mensagem: "Senha deve ter, pelo menos, 6 dígitos" })
+        }
+
+        if(erros.length > 0){
+            res.status(400).json(erros)
+        } else {
+            db.query(
+                `INSERT INTO colaborador (nome, senha) VALUES ('${this.nome}', '${this.senha}')`,
+                (error, result) => {
+                    error ? res.send(error) : res.json('Novo colaborador cadastrado com sucesso!');
+                }
+            );
+        }
     }
 
 }
